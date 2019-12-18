2b42a98957a4e7947e1376757b57984015c8f151
Eric Thieme-Garmann <erictg96@googlemail.com>
2019-12-18 08:05:19 +0100

    Added redirection logic to the web server

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+import requests
+
+
 app = Flask(__name__)
+
+logger_address = '192.168.4.14'
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/logger/<request>')
+def logger(request):
+    ### forward requests directly to the logger 
+    requestURL = logger_address + '/?'+request
+    r = requests.get(requestURL)
+    ### is r.text the right way to access to logger data? 
+    print(r.text)
+    ### return the returned json to the client
+    return (r.text)
 
 
 if __name__=='__main__':
